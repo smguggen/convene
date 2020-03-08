d4d65241fb95be8222b0d9c93f35dc01805f680f
smguggen <steven@directmail.io>
2020-03-08 13:43:40 -0500

    Fixd end event

--- bin/test.js
+++ bin/test.js
@@ -1,13 +1,34 @@
 #!/usr/bin/env node
 
+const assert = require('assert').strict;
+const fs = require('fs');
+const path = require('path');
+
 const Convene = require('../src/convene');
 const convene = new Convene({
     ext:'json'
 });
+
 convene.require(['a', 'b', 'c'], 'etc/a');
 convene.require(['d', 'e', 'f'], 'etc/b');
-convene.on('writing', (data) => data + '\n', true);
+//convene.on('writing', data => data + ',');
+
 convene.on('queueError', function(e) {
     throw new Error(e);
 });
+
+/*convene.on('merged', () => {
+    console.log('Merge Firing');
+    let root = process.cwd();
+
+    let key = fs.readFileSync(path.resolve(root, 'etc/key.js'));
+    let minKey = fs.readFileSync(path.resolve(root, 'etc/key.min.js'));
+    let res = fs.readFileSync(path.resolve(root, 'etc/result/convene.js'));
+    let minRes = fs.readFileSync(path.resolve(root, 'etc/result/convene.min.js'));
+
+    console.log(assert.equal(res, key, 'Files do not match'));
+    console.log(assert.equal(minRes, minKey, 'Minified Files do not match'));
+});*/
+
+
 convene.merge('convene', 'etc/result');
