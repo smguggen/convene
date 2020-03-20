44b4b9324e3b151b8da6b162014c7a71f44a65fa
smguggen <steven@directmail.io>
2020-03-20 17:46:18 -0500

    Fix test

--- bin/test.js
+++ bin/test.js
@@ -4,7 +4,4 @@ const Convene = require('../src/convene');
 const convene = new Convene();
 convene.queue({ 'etc/a': ['a', 'b', 'c'], 'etc/b': ['d', 'e', 'f'] }, 'require', 'json');
 convene.on('writing', (data) => data + '\n', true);
-convene.on('queueError', function(e) {
-    throw new Error(e);
-});
 convene.merge(process.cwd() + '/etc/result/convene.json', 'result');
